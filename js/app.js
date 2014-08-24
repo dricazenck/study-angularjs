eef5c205c8a4b025dd34aadc11ce428ffcc11ff7
dricazenck <dricazenck@gmail.com>
2014-08-24 18:14:25 -0300

    Add subtotal of the order

--- js/app.js
+++ js/app.js
@@ -5,13 +5,15 @@
 	app.controller("OrderController", function ($scope) {
 		this.nameApp = ' ---Order Drinks---';
 		this.nameApp = 'Order Drinks';
-		
+
+		this.order = createOrder('', "", [], 'Ok');
+				
 		this.products = [
 			createProduct(1, "Frozen Marguarita", 18.5, 'A'), 
 			createProduct(2, "Bloody Mary", 23.00, 'B'), 
 			createProduct(3, "Sex on The Beach", 25.00, 'C'), 
 			createProduct(4, "Marguarita", 15.35, 'C'), 
-			createProduct(5, "Cosmopolitan", 19.90, 'D')
+			createProduct(5, "Cosmopolitan", 21.50, 'D')
 		];
 
 		this.orders = [ 
@@ -20,6 +22,20 @@
 			createOrder(3, "02", [createItems(this.products[2], 1), createItems(this.products[3], 1)],'OK')
 		];
 
+		this.addItem = function(item) {
+			this.order.items.push(createItems(item.product, item.quantity));
+			delete item.quantity;
+			delete item.product;
+		};
+
+		this.finalizeOrder = function(order) {
+
+			this.orders.push(createOrder(this.orders.length+1, order.table, order.items, order.status));
+
+			order.items = [];
+			delete order.table;		
+		};
 	});
 
 })();
+
